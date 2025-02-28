2a1c8d88e6a1ae2920489ea371a1dd3c72e544a1
Levanbinh123 <levanbin1712@gmail.com>
2025-02-28 08:07:00 +0700

    1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+<div className='container-fluid'>
+    < a className='navbar-brand' helf='#'>binh</a>
+</div>
+        </nav>
+        </div>
+  );
+}
+
+export default App;
